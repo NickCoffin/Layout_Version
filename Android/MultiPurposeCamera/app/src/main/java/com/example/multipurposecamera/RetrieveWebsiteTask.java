814ac70534e16b8cb7429b072ee95c69d48599ea
Tyler <tuk54835@temple.edu>
2023-02-17 16:23:07 -0500

    Adding Android Project in Android folder

---
+++ Android/MultiPurposeCamera/app/src/main/java/com/example/multipurposecamera/RetrieveWebsiteTask.java
@@ -0,0 +1,41 @@
+package com.example.multipurposecamera;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class RetrieveWebsiteTask extends AsyncTask<Void, Void, String> {
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://4azfs5ytk7.execute-api.us-east-1.amazonaws.com/production/@connections");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return response.toString();
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.d("Result", result);
+    }
+}
---
+++ Android/MultiPurposeCamera/app/src/main/java/com/example/multipurposecamera/Website.java
@@ -0,0 +1,5 @@
+package com.example.multipurposecamera;
+
+public class Website {
+}
+
