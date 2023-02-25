a5e3ec36cb8b03d122b2a98cc547af516e29c7fa
NickCoffin <tuo76523@temple.edu>
2023-02-25 12:28:18 -0500

    RaspberryPi class shell

---
+++ Multi-Purpose-Camera/src/RaspberryPi.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RaspberryPi{
+    private String WIFI;
+    private List<Integer> Criteria = new ArrayList<Integer>();
+    public static void main(String args[]){
+        System.out.println("This is the main method");
+    }
+    public int Criteria_Met(List<int> criteria){}
+    public void Save_Video(){}
+    public void Send_Video(){}
+    public void Blue_Tooth_Connection(){}
+    public void WIFI_Connection(){}
+    public Alert Send_Alert( int alert ){}
+}
