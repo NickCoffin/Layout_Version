d6861392e2931a8d60d96d3fda8ccb84d0bc8038
NickCoffin <tuo76523@temple.edu>
2023-02-25 14:38:22 -0500

    Comments

--- Multi-Purpose-Camera/src/BabyMonitor.java
+++ Multi-Purpose-Camera/src/BabyMonitor.java
@@ -1,5 +1,16 @@
+/*
+ * BabyMonitor is one of the utilities of the multi-purpose-camera.
+ * It acts as a baby monitor by monitoring the intensity of the sound waves 
+ * using the audio device.
+ */
 public class BabyMonitor {
+    /*
+     * This Max_Decibels is the maximum number of decibels needed for a notification
+     * to go to the user
+     */
     private int Max_Decibels;
-
+    /* 
+     * is_Max_Decibel determines if the maximum number of decibels has been reached
+    */
     public void is_Max_Decibel( int max ){}
 }
