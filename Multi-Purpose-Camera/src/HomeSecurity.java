5cc4ec58984c48d9728e902841541d82f352bfb5
NickCoffin <tuo76523@temple.edu>
2023-02-25 14:15:53 -0500

    More class shells

---
+++ Multi-Purpose-Camera/src/HomeSecurity.java
@@ -0,0 +1,8 @@
+public class HomeSecurity {
+    private int Decibels;
+    private int Light_Exposure_Levels;
+
+    public int Decibel_Reading(){}
+    public void Motion_Detection(){}
+    public void Inactivity_Monitor(){}
+}
---
+++ Multi-Purpose-Camera/src/NightVision.java
@@ -0,0 +1,5 @@
+public class NightVision {
+    private int Light_Exposure_Level;
+
+    public int Light_Level( int lightLevel ){}
+}
